bee7076741ceb88c85d133c8c7352928fbf7feae
jjguillen <jjavierguillen@gmail.com>
2024-02-06 09:44:46 +0100

    api Juegos

--- torneos-app/routes/api.php
+++ torneos-app/routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JuegoController;
+use Illuminate\Support\Facades\Auth;
+
+use App\Http\Resources\JuegoResource;
+use App\Models\Juego;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +24,50 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::apiResource('/juegos', JuegoController::class);
+
+//RUTA CREACIÓN DE TOKEN: 
+Route::post('/tokens/create', function (Request $request) {
+    if (!Auth::attempt($request->only('email', 'password'))) {
+        return response()->json([
+          'message' => 'Unauthorized'
+        ], 401);
+      }
+      return response()->json([
+        'token' => $request->user()->createToken($request->email)->plainTextToken,
+        'message' => 'Success'
+      ]);
+});
+
+
+//RUTAS API
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/juegos', function () {
+        return JuegoResource::collection(Juego::all());
+    });
+
+    Route::get('/juegos/{id}', function (string $id) {
+        return new JuegoResource(Juego::findOrFail($id));
+    });
+
+    Route::delete('/juegos/{id}', function (string $id) {
+        if (Juego::findOrFail($id)) {
+            Juego::destroy($id);
+            return ['message' => 'Juego deleted'];
+        } 
+    });
+
+    Route::post('/juegos', function (Request $request) {
+        $juego = new Juego;
+        $juego->nombre = $request->nombre;
+        $juego->plataforma = $request->plataforma;
+        $juego->edadR = $request->edadR;
+        $juego->nota = $request->nota;
+        $juego->save();
+        return ['message' => 'Juego created',
+                'juego' => $juego];
+    });
+
+
+}); 
+
+
